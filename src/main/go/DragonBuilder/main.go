0ba37d52e611899898ab565ae0a0c5f1be6ce304
Aneesh Anil <aneesh@geektrust.in>
2024-06-28 16:00:38 +0530

    basic boilerplate for reading file.

--- src/main/go/DragonBuilder/main.go
+++ src/main/go/DragonBuilder/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"errors"
-	"log/slog"
+	"log"
 	"os"
-	"strings"
+	"time"
 )
 
 // const inputFilePath = "./measurements.txt"
 
 func main() {
-	slog.Info("Starting 1brc processing.")
-	slog.Info(strings.Join(os.Args, ", "))
+	start := time.Now()
 	if len(os.Args) < 2 {
-		slog.Error("Provide a path to the file containing measurements.")
-		return
+		log.Fatalf("Input file path not given.\n\nExample: %s <path to file>", os.Args[0])
 	}
 	inputFilePath := os.Args[1]
 	if _, err := os.Stat(inputFilePath); errors.Is(err, os.ErrNotExist) {
-		slog.Error("Input file not found.", "location", inputFilePath)
+		log.Fatalf("File not found: %s", inputFilePath)
 		return
 	}
+
+	fileReader, err := os.OpenFile(inputFilePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error while reading file %s : %v", inputFilePath, err)
+		return
+	}
+	defer fileReader.Close()
+
+	fileScanner := bufio.NewScanner(fileReader)
+	for fileScanner.Scan() {
+	}
+
+	total_time_taken := time.Since(start)
+	log.Printf("Done. Took %s", total_time_taken.String())
 
 }
